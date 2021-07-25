3dff1b41fca4b5c53ef139392a772d16462c8e2a
TinaAntony <antony_tina@yahoo.co.in>
2021-07-25 22:15:08 +1000

    first commit

--- src/test/java/websitehosting/cicdautomation/wh_test1.java
+++ src/test/java/websitehosting/cicdautomation/wh_test1.java
@@ -4,18 +4,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class wh_test1 {
 	public static WebDriver driver;
-	
+	@Test
 	public void test1()
 	{
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.get("http://localhost:8080/websitehosting");
-		String tagname = driver.findElement(By.tagName("h1")).getText();
+		String tagname = driver.findElement(By.tagName("h2")).getText();
 		System.out.println("CICD - Automation");
 		Assert.assertEquals(tagname, "We are trying Continuous integration and development");			
 		
